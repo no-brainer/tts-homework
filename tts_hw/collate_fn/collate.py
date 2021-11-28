f7ea1cafff0fd71f461fc2678465a4af90162c2d
Polina Guseva <p.guseva00@gmail.com>
2021-11-29 01:00:22 +0300

    add lj dataset and collator

---
+++ tts_hw/collate_fn/collate.py
@@ -0,0 +1,33 @@
+import logging
+from typing import List, Tuple, Dict
+
+import torch
+from torch.nn.utils.rnn import pad_sequence
+
+logger = logging.getLogger(__name__)
+
+
+class CollatorFn:
+
+    def __call__(self, instances: List[Tuple]) -> Dict:
+        waveform, waveform_length, transcript, tokens, token_lengths = list(
+            zip(*instances)
+        )
+
+        waveform = pad_sequence([
+            waveform_[0] for waveform_ in waveform
+        ]).transpose(0, 1)
+        waveform_length = torch.cat(waveform_length)
+
+        tokens = pad_sequence([
+            tokens_[0] for tokens_ in tokens
+        ]).transpose(0, 1)
+        token_lengths = torch.cat(token_lengths)
+
+        return {
+            "waveform": waveform,
+            "waveform_lengths": waveform_length,
+            "text": transcript,
+            "text_encoded": tokens,
+            "text_encoded_lengths": token_lengths,
+        }
---
+++ tts_hw/datasets/__init__.py
@@ -0,0 +1,5 @@
+from tts_hw.datasets.lj_dataset import LJSpeechDataset
+
+__all__ = [
+    "LJSpeechDataset",
+]

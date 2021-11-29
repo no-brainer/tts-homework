7df41cc164c7b5a65a8f9b7c0420f58c32ce306d
Polina Guseva <p.guseva00@gmail.com>
2021-11-29 13:17:19 +0300

    add featurizer

---
+++ tts_hw/featurizer/featurizer.py
@@ -0,0 +1,46 @@
+import librosa
+import torch
+import torch.nn as nn
+import torchaudio
+
+
+class MelSpectrogram(nn.Module):
+
+    def __init__(self, sr, win_length, hop_length, n_fft, f_min, f_max, n_mels, power):
+        super(MelSpectrogram, self).__init__()
+
+        self.mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+            sample_rate=sr,
+            win_length=win_length,
+            hop_length=hop_length,
+            n_fft=n_fft,
+            f_min=f_min,
+            f_max=f_max,
+            n_mels=n_mels
+        )
+
+        # The is no way to set power in constructor in 0.5.0 version.
+        self.mel_spectrogram.spectrogram.power = power
+
+        # Default `torchaudio` mel basis uses HTK formula. In order to be compatible with WaveGlow
+        # we decided to use Slaney one instead (as well as `librosa` does by default).
+        mel_basis = librosa.filters.mel(
+            sr=sr,
+            n_fft=n_fft,
+            n_mels=n_mels,
+            fmin=f_min,
+            fmax=f_max
+        ).T
+        self.mel_spectrogram.mel_scale.fb.copy_(torch.tensor(mel_basis))
+
+    def forward(self, audio: torch.Tensor) -> torch.Tensor:
+        """
+        :param audio: Expected shape is [B, T]
+        :return: Shape is [B, n_mels, T']
+        """
+
+        mel = self.mel_spectrogram(audio) \
+            .clamp_(min=1e-5) \
+            .log_()
+
+        return mel

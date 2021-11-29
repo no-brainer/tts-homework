597849a95f85b9908df5c513f2efdfed649856f4
Polina Guseva <p.guseva00@gmail.com>
2021-11-29 14:38:09 +0300

    fix names in loss

--- tts_hw/loss/fast_speech_loss.py
+++ tts_hw/loss/fast_speech_loss.py
@@ -15,19 +15,19 @@ class FastSpeechLoss(nn.Module):
         self.duration_pred_coef = duration_pred_coef
 
     def forward(self, *args, **kwargs) -> Tuple[Tensor, Dict]:
-        mel_tgt = kwargs.get("mel_tgt")
-        mel_lens = kwargs.get("mel_lengths")
+        mel_tgt = kwargs.get("melspec")
+        mel_lens = kwargs.get("melspec_lengths")
         mel_mask = get_mask_from_lengths(mel_lens, mel_tgt.size(1))
 
-        mel_pred = kwargs.get("mel_pred")
+        mel_pred = kwargs.get("melspec_preds")
         mel_pred = F.pad(mel_pred, (0, 0, 0, mel_tgt.size(1) - mel_pred.size(1), 0, 0))
         mel_loss = (F.mse_loss(mel_pred, mel_tgt, reduction="none") * mel_mask).sum() / mel_mask.sum()
 
-        dur_tgt = torch.log1p(kwargs.get("dur_tgt"))
-        dur_lens = kwargs.get("phoneme_lengths")
+        dur_tgt = torch.log1p(kwargs.get("durations") * mel_tgt.size(1))
+        dur_lens = kwargs.get("text_encoded_lengths")
         dur_mask = get_mask_from_lengths(dur_lens, dur_tgt.size(1))
 
-        dur_log_pred = kwargs.get("dur_log_pred")
+        dur_log_pred = kwargs.get("log_durations_pred")
         duration_loss = (F.mse_loss(dur_log_pred, dur_tgt, reduction="none") * dur_mask) / torch.sum(dur_mask)
 
         loss = mel_loss + self.duration_pred_coef * duration_loss

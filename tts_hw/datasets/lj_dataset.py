9784fcd13c75a2ff465b5ba8f21573d5b35e9fc9
Polina Guseva <p.guseva00@gmail.com>
2021-11-29 12:57:45 +0300

    add train/test split to ljspeech

--- tts_hw/datasets/lj_dataset.py
+++ tts_hw/datasets/lj_dataset.py
@@ -4,11 +4,22 @@ import torchaudio
 
 class LJSpeechDataset(torchaudio.datasets.LJSPEECH):
 
-    def __init__(self, root):
+    def __init__(self, root, mode):
         super().__init__(root=root)
         self._tokenizer = torchaudio.pipelines.TACOTRON2_GRIFFINLIM_CHAR_LJSPEECH.get_text_processor()
+        self.mode = mode
+
+    def __len__(self):
+        full_size = super().__len__()
+        train_size = int(0.8 * full_size)
+        if self.mode == "train":
+            return train_size
+        return full_size - train_size
 
     def __getitem__(self, index: int):
+        if self.mode != "train":
+            index += int(0.8 * super().__len__())
+
         waveform, _, _, transcript = super().__getitem__(index)
         waveform_length = torch.tensor([waveform.shape[-1]]).int()
 
